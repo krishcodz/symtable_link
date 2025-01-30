ac272b7cc5bc41f62de343f0ec62de14c00c8871
krishcodz <krishnax711@gmail.com>
2025-01-30 10:53:44 +0530

    link functions added

--- include/SymTable.h
+++ include/SymTable.h
@@ -1,6 +1,12 @@
 #pragma once
 
-struct Node;
+struct Node
+{
+    const char *key;
+    const void *value;
+    Node *next;
+};
+
 struct SymTable
 {
     struct Node *root;
@@ -10,3 +16,15 @@ struct SymTable
 typedef struct SymTable *SymTable_t;
 
 SymTable_t SymTable_new(void);
+
+void SymTable_free(SymTable_t oSymTable);
+
+int SymTable_getLength(SymTable_t oSymTable);
+
+int SymTable_put(SymTable_t oSymTable,
+                 const char *pcKey,
+                 const void *pvValue);
+
+void *SymTable_replace(SymTable_t oSymTable,
+                       const char *pcKey,
+                       const void *pvValue);
--- src/symtable_link.cpp
+++ src/symtable_link.cpp
@@ -1,12 +1,5 @@
 #include "SymTable.h"
 
-struct Node
-{
-    char *key;
-    void *value;
-    Node *next;
-};
-
 SymTable_t SymTable_new(void)
 {
     SymTable_t table = new SymTable;
@@ -15,4 +8,79 @@ SymTable_t SymTable_new(void)
     table->root = nullptr;
     table->len = 0;
     return table;
+}
+
+void SymTable_free(SymTable_t oSymTable)
+{
+    delete (oSymTable);
+}
+
+int SymTable_getLength(SymTable_t oSymTable)
+{
+    return oSymTable->len;
+}
+
+int SymTable_put(SymTable_t oSymTable,
+                 const char *pcKey,
+                 const void *pvValue)
+{
+
+    if (oSymTable == nullptr)
+        return 0;
+
+    Node *newNode = new Node;
+    newNode->key = pcKey;
+    newNode->value = pvValue;
+    newNode->next = nullptr;
+
+    if (oSymTable->root == nullptr)
+    {
+        oSymTable->root = newNode;
+        oSymTable->len++;
+        return 1;
+    }
+
+    Node *current = oSymTable->root;
+    while (current->next != nullptr)
+    {
+        current = current->next;
+    }
+
+    current->next = newNode;
+    oSymTable->len++;
+    return 1;
+}
+
+void *SymTable_replace(SymTable_t oSymTable,
+                       const char *pcKey,
+                       const void *pvValue)
+{
+
+    if (oSymTable == nullptr)
+        return nullptr;
+
+    Node *current = oSymTable->root;
+
+    while (current != nullptr)
+    {
+        const char *s1 = current->key;
+        const char *s2 = pcKey;
+
+        while (*s1 && *s2 && (*s1 == *s2))
+        {
+            s1++;
+            s2++;
+        }
+
+        if (*s1 == *s2)
+        {
+            void *oldValue = (void *)current->value;
+            current->value = pvValue;
+            return oldValue;
+        }
+
+        current = current->next;
+    }
+
+    return nullptr;
 }
